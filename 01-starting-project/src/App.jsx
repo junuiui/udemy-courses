3eb3425cf86846634528c6bc48c01c4584aaaae9
Junui Hong <jha357@sfu.ca>
2025-08-12 11:28:06 -0700

    added

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,19 +1,46 @@
+import reactImg from './assets/react-core-concepts.png'
+
+const reactDescription = ['Fundamental', 'Crucial', 'Core'];
+
+/**
+ * Generate random number
+ * @param {*} max 
+ * @returns 
+ */
+function genRandomInt(max){
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+
+    const description = reactDescription[genRandomInt(2)];
+
+    return (
+        <header>
+            <img src={reactImg} alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                <b>{description}</b> React concepts you will need for almost any app you are
+                going to build!
+            </p>
+        </header>
+    );
+}
+
+function Intro(){
+    return (<h2>Time to get started!</h2>);
+}
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            {/* self closing, must have / at the end */}
+            <Header/>
+            <main>
+                <Intro/>
+            </main>
+        </div>
+    );
 }
 
 export default App;
