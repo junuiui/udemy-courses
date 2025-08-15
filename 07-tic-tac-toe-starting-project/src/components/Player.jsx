d762d32594d1a3120e4c8cd53d669ddace496c79
Junui Hong <jha357@sfu.ca>
2025-08-14 20:14:14 -0700

    project updated

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+export default function Player({name, symbol}) {
+
+    const [ updatedName, updateName ] = useState(name);
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing );
+    }
+
+    function handleUpdatingName(event) {
+        updateName(event.target.value);
+    }
+
+    let playerName = <span className="player-name">{updatedName}</span>;
+    let buttonName = 'EDIT'
+    if (isEditing) {
+        playerName = <input type='text' required value={updatedName} onChange={handleUpdatingName}/>
+        buttonName = 'SAVE'
+    }
+    else {
+        buttonName = 'EDIT'
+    }
+
+    return (
+        <>
+            <li>
+                <span className='player'>
+                    {playerName}
+                    <span className="player-symbol">{symbol}</span>
+                </span>
+
+                <button onClick={handleEditClick}>{buttonName}</button>
+            </li>
+        </>
+    )
+}
