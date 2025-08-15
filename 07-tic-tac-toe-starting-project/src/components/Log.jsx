db0b270b021231176010ed8e59dddd87da8e52cf
Junui Hong <jha357@sfu.ca>
2025-08-15 13:54:48 -0700

    added

---
+++ 07-tic-tac-toe-starting-project/src/components/Log.jsx
@@ -0,0 +1,5 @@
+export default function Log( { turns } ) {
+    return <ol id="log">
+        {turns.map(turn => <li key={`${turn.square.col}`}>{turn.player} : {turn.square.row}, {turn.square.col} selected</li>)}
+    </ol>
+}
