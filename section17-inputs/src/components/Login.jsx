835216256588944a60573a08da88541e24959099
junuiui <jhong980821@outlook.com>
2025-09-12 14:20:26 -0700

    added

--- section17-inputs/src/components/Login.jsx
+++ section17-inputs/src/components/Login.jsx
@@ -1,23 +1,43 @@
+import { useState } from "react";
 import { useRef } from "react";
 
 export default function Login() {
+
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
 
   const email = useRef();
   const password = useRef();
 
   function handleSubmit(event) {
+    event.preventDefault();
     console.log(
       '%cLogin.jsx%c Submit Button Pressed',
       'color: yellow; background: red; font-weight: bold; padding: 2px 4px;',
       'color: green; background: yellow; font-weight: bold;'
     );
 
+
+
     // Ref approach!
+    // downside: resetting is much harder
+    // resetting dom is not recommended
     const enteredEmail = email.current.value;
     const enteredPW = password.current.value;
-    console.log(`   ID: ${enteredEmail}`)
-    console.log(`   PW: ${enteredPW}`)
-    event.preventDefault();
+    console.log(`   ID: ${enteredEmail}`);
+    console.log(`   PW: ${enteredPW}`);
+
+    // Validation
+    const emailIsValid = enteredEmail.includes('@');
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    console.log(emailIsInvalid)
+
+    setEmailIsInvalid(false);
+
+    console.log('sending HTTP request....')
   }
 
   function handleReset() {
@@ -39,13 +59,15 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input
-            id="email" type="email" name="email" ref={email}/>
+          <input id="email" type="email" name="email" ref={ email } />
+          <div className="control-error">
+            { emailIsInvalid && <p>Please Enter Valid email</p> }
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" ref={password}/>
+          <input id="password" type="password" name="password" ref={ password } />
         </div>
       </div>
 
--- section17-inputs/src/components/StateLogin.jsx
+++ section17-inputs/src/components/StateLogin.jsx
@@ -1,4 +1,86 @@
-const [enteredValues, setEnteredValues] = useState({
-  email: '',
-  password: '',
-});
+import { useState } from "react";
+
+export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(enteredValues)
+  }
+
+  function handleReset() {
+    console.log(
+      '%cLogin.jsx%c Reset Button Pressed',
+      'color: yellow; background: red; font-weight: bold; padding: 2px 4px;',
+      'color: green; background: yellow; font-weight: bold;'
+    );
+
+    // Todo
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }))
+
+    setDidEdit(prev => ({
+      ...prev,
+      [identifier]: false,
+    }))
+  }
+
+  // when lose focus!
+  function handleInputBlur(identifier) {
+    setDidEdit(prev => ({
+      ...prev,
+      [identifier]: true
+    }))
+  }
+
+
+  return (
+    // form onSubmit is in here, not on the Submit button!
+    <form onSubmit={ handleSubmit }>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onBlur={() => handleInputBlur('email')}
+            onChange={ (event) => handleInputChange('email', event.target.value) } // Using onchange, error detected too EARLY
+            value={ enteredValues.email } />
+          <div className="control-error">{ emailIsInvalid && <p>Invalid email! has to include '@'</p> }</div>
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password" />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat" onClick={ handleReset }>Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
