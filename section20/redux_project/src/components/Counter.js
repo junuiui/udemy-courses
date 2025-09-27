ae4225d6dd2cbb640a75b6d6f0f5cd40d29a9e8e
Junui Hong <jha357@sfu.ca>
2025-09-26 23:28:37 -0700

    redux updated

--- section20/redux_project/src/components/Counter.js
+++ section20/redux_project/src/components/Counter.js
@@ -1,12 +1,39 @@
+import { useSelector, useDispatch } from 'react-redux'
+
 import classes from './Counter.module.css';
+import { counterActions } from '../store/index'
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment(1));
+  }
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  }
+
+  const increaseByFive = () => {
+    dispatch(counterActions.increment(5));
+  }
+
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggle());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={decrementHandler}>-</button>
+        <button onClick={incrementHandler}>+</button>
+        <button onClick={increaseByFive}>+ 5</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
---
+++ section20/redux_project/src/store/index.js
@@ -0,0 +1,54 @@
+// import { createStore,  } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit'
+
+
+const initialState = { counter: 0, showCounter: true }
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialState, // or automatically js to figure out
+  reducers: {
+    increment(state, action) { state.counter += action.payload; },
+    decrement(state, action) { state.counter -= action.payload; },
+    toggle(state) { state.showCounter = !state.showCounter },
+  }
+
+});
+
+// ===============
+// replaced by '@reduxjs/toolkit'
+// ===============
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === 'increment') {
+//     return {
+//       // NEVER mutate the existing state. ALWAYS overwrite.
+//       counter: state.counter + action.value,
+//       showCounter: state.showCounter
+//     };
+//   }
+
+//   if (action.type === 'decrement') {
+//     return {
+//       counter: state.counter - action.value,
+//       showCounter: state.showCounter
+//     };
+//   }
+
+//   if (action.type === 'toggle') {
+//     return {
+//       counter: state.counter,
+//       showCounter: !state.showCounter
+//     };
+//   }
+
+//   return state;
+// };
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+
+});
+
+export const counterActions = counterSlice.actions;
+
+export default store;
