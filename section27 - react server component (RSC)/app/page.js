b99d48a0e0fe1d18a59fedb200048b161f413053
Junui Hong <jha357@sfu.ca>
2025-10-28 15:20:29 -0700

    server actions dta, data fetching added

--- section27 - react server component (RSC)/app/page.js
+++ section27 - react server component (RSC)/app/page.js
@@ -1,11 +1,12 @@
 import ClientDemo from "@/components/ClientDemo";
+import DataFetchingDemo from "@/components/DataFetchingDemo";
 import RSCDemo from "@/components/SRCDemo";
+import ServerActionsDemo from "@/ServerActionsDemo";
 
 export default function Home() {
   return (
     <main>
-      <RSCDemo />
-      <ClientDemo />
+      <ServerActionsDemo />
     </main>
   );
 }
