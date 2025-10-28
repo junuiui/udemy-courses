209a11f93188df7025718a270c90748c98875189
Junui Hong <jha357@sfu.ca>
2025-10-28 15:13:45 -0700

    client demo, src demo added

---
+++ section27 - react server component (RSC)/app/page.js
@@ -0,0 +1,11 @@
+import ClientDemo from "@/components/ClientDemo";
+import RSCDemo from "@/components/SRCDemo";
+
+export default function Home() {
+  return (
+    <main>
+      <RSCDemo />
+      <ClientDemo />
+    </main>
+  );
+}
