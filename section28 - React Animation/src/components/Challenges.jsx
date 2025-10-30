29cf4d20a6780fe87d41890cf82dafbed4d1f889
Junui Hong <jha357@sfu.ca>
2025-10-30 16:15:48 -0700

    tab animation added

--- section28 - React Animation/src/components/Challenges.jsx
+++ section28 - React Animation/src/components/Challenges.jsx
@@ -44,6 +44,8 @@ export default function Challenges() {
           {displayedChallenges.length > 0 && (
             <motion.ol
               key='list' // if more than 1, should add a key
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
               exit={{ y: -30, opacity: 0 }}
               className="challenge-items"
             >
