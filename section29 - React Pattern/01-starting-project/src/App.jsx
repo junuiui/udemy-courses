6874c0995860a55355bb827602a9f4e9fff7c80e
junuiui <jhong980821@outlook.com>
2025-11-10 13:21:22 -0800

    open, close by using context is added

--- section29 - React Pattern/01-starting-project/src/App.jsx
+++ section29 - React Pattern/01-starting-project/src/App.jsx
@@ -1,5 +1,32 @@
+import Accordion from "./components/Accordion/Accordion";
+import AccordionItem from "./components/Accordion/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return <main>
+    <section>
+      <h2>Why work with us?</h2>
+      <Accordion className='accordion'>
+        {/* First */ }
+        <AccordionItem title="We got 20 years of experience" className="accordion-item" id="first">
+          <article>
+            <p>
+              You can&apos;t go wrong with us!
+            </p>
+          </article>
+        </AccordionItem>
+
+        {/* Second */ }
+        <AccordionItem title="We Are..." className="accordion-item" id="second">
+          <article>
+            <p>
+              Second...
+            </p>
+          </article>
+        </AccordionItem>
+
+      </Accordion>
+    </section>
+  </main>;
 }
 
 export default App;
