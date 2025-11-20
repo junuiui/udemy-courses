a02fc90582fa4e7c215fba7670ac6753d00c18e2
junuiui <jhong980821@outlook.com>
2025-11-20 14:18:51 -0800

    tuples added

--- Typecript/05_arrayAndEnum/index.ts
+++ Typecript/05_arrayAndEnum/index.ts
@@ -28,9 +28,22 @@ let airplane: AirPlanes = [
 // Tuples (array with fixed shape)
 let person: [string, string, number] = ["John", "Doe", 18];
 
+/**
+ * firstName, lastName, age
+ */
+type User = [string, string, number, string?]; // last string? is optional!!
+let user: User = ["f", "l", 20, "fl@example.com"]
+
+type listOfStudents = [number, boolean, ...string[]]
+const passingStudents: listOfStudents = [3, true, "John", "Stella", "Mark"]
+
+type StringBooleansNumber = [string, ...boolean[], number]
+const stringboolsnumbers: StringBooleansNumber = ["h", true, true, false, false, 10]
+
 
 ////////////
 // Readonly Arrays and Tuples
+
 
 
 ////////////
