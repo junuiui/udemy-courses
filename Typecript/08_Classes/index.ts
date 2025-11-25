6814441514aa5b4a2db203a49a3b1f2008c59812
junuiui <jhong980821@outlook.com>
2025-11-25 15:28:05 -0800

    solution added

--- Typecript/08_Classes/index.ts
+++ Typecript/08_Classes/index.ts
@@ -59,20 +59,52 @@ const admin: Admin = new Admin("AdminName", "admin@ad.com", 4, "Male");
  * * - author (string, required)
  * * - yearPublished (number, optional)
  * * - ISBN (string, readonly)
- *
- * TODO: 2. Define a constructor function to initialize the Book class with title, author,yearPublished, and ISBN.
- *
- * TODO: 3. Ensure that the constructor function uses the this keyword to assign values to the class properties.
- *
- * TODO: 4. Create an instance of the Book class and log its details.
- *
- * TODO: 5. Create a function logBookDetails that takes an instance of Book as a parameter and logs its details.
- *
- * TODO: 6. Create a subclass EBook that extends the Book class. Add the following properties:
- * * - fileSize (number, required)
- * * - format (string, required)
- *
- * TODO:7. Use the super method to call the constructor of the parent class Book from the EBook class.
- *
- * TODO: 8. Ensure that the yearPublished property in the Book class is optional and the ISBN property is readonly.
  */
+
+class Book {
+    public title: string;
+    public author: string;
+    public yearPublished?: number;
+    readonly ISBN: string
+
+    //  * TODO: 2. Define a constructor function to initialize the Book class with title, author,yearPublished, and ISBN.
+    //  * TODO: 3. Ensure that the constructor function uses the this keyword to assign values to the class properties.
+    constructor(title: string, author: string, ISBN: string, yearPublished?: number) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        if (yearPublished) { this.yearPublished = yearPublished; }
+
+    }
+}
+
+//  * TODO: 4. Create an instance of the Book class and log its details.
+const book1 = new Book("Harry Potter", "James Mudric", "isbn-v394kv", 1998)
+const book2 = new Book("Hunger Games", "Kate Yak", "isbn-v1433kv")
+
+//  * TODO: 5. Create a function logBookDetails that takes an instance of Book as a parameter and logs its details.
+function logBookDetails(book: Book): void {
+    console.log(book.author)
+    console.log(book.title)
+    console.log(book.ISBN)
+    book.yearPublished ? console.log(book.yearPublished) : console.log();
+}
+
+//  * TODO: 6. Create a subclass EBook that extends the Book class. Add the following properties:
+//  * * - fileSize (number, required)
+//  * * - format (string, required)
+class EBook extends Book {
+    fileSize: number;
+    format: string;
+
+    //  * TODO:7. Use the super method to call the constructor of the parent class Book from the EBook class.
+    constructor(title: string, author: string, ISBN: string, fileSize: number, format: string, yearPublished?: number) {
+        super(title, author, ISBN, yearPublished);
+        this.fileSize = fileSize;
+        this.format = format;
+    }
+
+}
+
+//  * TODO: 8. Ensure that the yearPublished property in the Book class is optional and the ISBN property is readonly.
+
