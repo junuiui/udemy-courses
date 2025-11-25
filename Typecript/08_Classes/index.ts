719ad759821740c7265ce10bd961db51a8cfe15a
junuiui <jhong980821@outlook.com>
2025-11-25 15:18:03 -0800

    practice added

--- Typecript/08_Classes/index.ts
+++ Typecript/08_Classes/index.ts
@@ -49,4 +49,30 @@ class Admin extends User {
 
 const admin: Admin = new Admin("AdminName", "admin@ad.com", 4, "Male");
 
-// Practice
+/**
+ * * Practice Problem
+ * * You are building a simple library management system.
+ * * Implement the following requirements using TypeScript:
+ *
+ * TODO: 1. Create a class Book with the following properties:
+ * * - title (string, required)
+ * * - author (string, required)
+ * * - yearPublished (number, optional)
+ * * - ISBN (string, readonly)
+ *
+ * TODO: 2. Define a constructor function to initialize the Book class with title, author,yearPublished, and ISBN.
+ *
+ * TODO: 3. Ensure that the constructor function uses the this keyword to assign values to the class properties.
+ *
+ * TODO: 4. Create an instance of the Book class and log its details.
+ *
+ * TODO: 5. Create a function logBookDetails that takes an instance of Book as a parameter and logs its details.
+ *
+ * TODO: 6. Create a subclass EBook that extends the Book class. Add the following properties:
+ * * - fileSize (number, required)
+ * * - format (string, required)
+ *
+ * TODO:7. Use the super method to call the constructor of the parent class Book from the EBook class.
+ *
+ * TODO: 8. Ensure that the yearPublished property in the Book class is optional and the ISBN property is readonly.
+ */
