e5bc8a9de68d464af6449583504ae8c682551914
Junui Hong <jha357@sfu.ca>
2025-12-03 10:30:35 -0800

    function contructor added

--- Typecript/11_Prototypes and Objects/index.js
+++ Typecript/11_Prototypes and Objects/index.js
@@ -12,6 +12,13 @@ const book = {
         this.authors.forEach(function (author) {
             console.log(this.title, " - ", author) // this infers the function inside
         }, this) // this leads binding this
+    },
+
+    printAuthors2() {
+        // => arrow makes to refer the parent object
+        this.authors.forEach((author) => {
+            console.log(this.title, " - ", author)
+        })
     }
 }
 
@@ -25,4 +32,44 @@ book.stopreading = function () {
 // window, global
 function watchMovie() {
     console.log(this);
+}
+
+// Constructor Functions
+class User {
+    public name;
+    public email;
+    public points;
+
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        this.points = 0;
+    }
+
+    login() {
+        console.log(this.name, " Logged in")
+    }
+
+    logout() {
+        console.log(this.name, " Logged out")
+    }
+
+    addPoints() {
+        this.points++;
+    }
+}
+
+const user = new User('N', '123@ww.com');
+
+console.log(user) // functions are NOT in User object. they are in Prototype
+
+// functions are in User object.
+function User2(name, email) {
+    this.name = name;
+    this.email = email;
+    this.points = 0;
+
+    this.login = () => { };
+    this.logout = () => { };
+    this.addpoints = () => { };
 }
