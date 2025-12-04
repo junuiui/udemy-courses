f29a66dc6a2285e7e88fd04197698f6e8ad190a0
Junui Hong <jha357@sfu.ca>
2025-12-04 10:51:12 -0800

    initialization added

--- Typecript/12_Decorators_stage3/index.ts
+++ Typecript/12_Decorators_stage3/index.ts
@@ -1,7 +1,28 @@
 // 1. Creating Decorator
 function methodLogger(originalMethod: any, context: any) {
-    console.log(originalMethod)
-    console.log(context);
+
+    const methodName = context.name;
+
+    if (context.private) {
+        throw new Error("Cannot ... ")
+    }
+
+    context.addInitializer(function (this: any) {
+        console.log(this);
+        this[methodName] = this[methodName].bind(this);
+    })
+
+    // to return new function
+    function replacementMethod(this: any, ...args: any[]) {
+        console.log(args); // [params]
+        console.log(this); // Person {name: ...}
+
+        // invocation
+        const result = originalMethod.call(this, ...args);
+
+
+    }
+    return replacementMethod;
 }
 
 // function classDecorator(target: any, context: any) {
@@ -12,11 +33,18 @@ function methodLogger(originalMethod: any, context: any) {
 // @classDecorator
 class Person {
     constructor(public name: string) {
-        
+
     }
 
     @methodLogger
-    greet() {
-        console.log(`Hello my name is ${this.name}`)
+    greet(str: string) : void {
+        console.dir(this)
+        console.log(`${str} my name is ${this.name}`)
     }
 }
+
+let user: Person = new Person("Jun");
+user.greet("Hello"); // replaced
+
+const greet = user.greet("Hello");
+greet("Hello");
